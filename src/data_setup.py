f49a43bc35a3952da8001667da180d29cc6d3295
LeoPCaucheteux <leopaul.caucheteux@gmail.com>
2023-06-13 14:40:32 +0100

    Change data setup to take only 0-45 days

--- src/data_setup.py
+++ src/data_setup.py
@@ -326,40 +326,44 @@ def Transform_Data_For_DT(df,SIZE:60, enable_encode:False):
     Returns:
         df_train: new data frame with prior data points and a target sales
     """
-    COLUMNS = ['t{}'.format(x) for x in range(SIZE)] + ['target']
-    df_train= []
-    fam_list= []
-    sto_list = []
-    date_list=[]
-    family_list =df['family'].unique()
-    store_list =df['store_nbr'].unique()
-    for fam in family_list:
-        for sto in store_list:
-            tmp= df[(df['family']==fam) & (df['store_nbr']==sto)]
-            tmp=tmp.reset_index()
-            if tmp.shape[0]>0:
-                for i in range(SIZE, tmp.shape[0]):
-                    df_train.append(tmp.loc[i-SIZE:i, 'sales'].tolist())
-                    fam_list.append(fam)
-                    sto_list.append(sto)
-                    date_list.append(tmp.loc[i, 'date'])
-    df_train = pd.DataFrame(df_train, columns=COLUMNS)
-    df_train['family']=fam_list
-    df_train['store_nbr']=sto_list
-    df_train['date']=date_list
-    df_train['date']= pd.to_datetime(df_train['date'])
-    df_train["year"] = df_train.date.dt.year
-    df_train["month"] = df_train.date.dt.month
-    df_train["daynumber"] = df_train.date.dt.day
-    df_train['day_of_week'] = df_train['date'].dt.dayofweek
-    columns= ['family', 'store_nbr']
-    if enable_encode:
-        columns= ['family']
-        for col in columns:
-            le=LabelEncoder()
-            df_train[col]=le.fit_transform(df_train[col])
-    
-    return(df_train)
+    if SIZE <=15:
+        print('SIZE parameter is too small, pick a bigger integer')
+        return()
+    else:
+        COLUMNS = ['t{}'.format(x) for x in range(SIZE)] + ['target']
+        df_train= []
+        fam_list= []
+        sto_list = []
+        date_list=[]
+        family_list =df['family'].unique()
+        store_list =df['store_nbr'].unique()
+        for fam in family_list:
+            for sto in store_list:
+                tmp= df[(df['family']==fam) & (df['store_nbr']==sto)]
+                tmp=tmp.reset_index()
+                if tmp.shape[0]>0:
+                    for i in range(SIZE, tmp.shape[0]):
+                        df_train.append(tmp.loc[i-SIZE:i-15, 'sales'].tolist())
+                        fam_list.append(fam)
+                        sto_list.append(sto)
+                        date_list.append(tmp.loc[i, 'date'])
+        df_train = pd.DataFrame(df_train, columns=COLUMNS)
+        df_train['family']=fam_list
+        df_train['store_nbr']=sto_list
+        df_train['date']=date_list
+        df_train['date']= pd.to_datetime(df_train['date'])
+        df_train["year"] = df_train.date.dt.year
+        df_train["month"] = df_train.date.dt.month
+        df_train["daynumber"] = df_train.date.dt.day
+        df_train['day_of_week'] = df_train['date'].dt.dayofweek
+        columns= ['family', 'store_nbr']
+        if enable_encode:
+            columns= ['family']
+            for col in columns:
+                le=LabelEncoder()
+                df_train[col]=le.fit_transform(df_train[col])
+        
+        return(df_train)
 
 def clean_train(train):
     """
@@ -396,7 +400,7 @@ def DT_features(df, enable_encode:False):
     temp=df.drop(columns={'family','store_nbr','date','year','month','daynumber','day_of_week'})
     df_feats=pd.DataFrame()
     df_feats['prev_1'] = temp.iloc[:,-2] #Here -2 as -1 is a target
-    for win in [2, 3, 5, 7, 10, 14, 21, 28, 56]: 
+    for win in [2, 3, 5, 7, 10, 14, 21, 28]: 
         #Interval of study, big window are good against noisy data, small window perform best for sharp trend
         tmp = temp.iloc[:,-1-win:-1]
         #General statistics for base level
@@ -412,7 +416,8 @@ def DT_features(df, enable_encode:False):
         df_feats['avg_diff_{}'.format(win)] = (tmp - tmp.shift(1, axis=1)).mean(axis=1)
         #df_feats['avg_div_{}'.format(win)] = (tmp / tmp.shift(1, axis=1)).mean(axis=1) --Not sure of this one
         del tmp
-    for win in [2, 3, 4, 8]:
+    for win in [2, 3, 4]:
+        #Check ADF test for seasonality
         tmp = temp.iloc[:,-1-win*7:-1:7] #7 for week
         #Features for weekly seasonality
         df_feats['week_mean_prev_{}'.format(win)] = tmp.mean(axis=1)
